8795a65dd90de10a3b777ff3dd66964c284cba62
kmc2203 <alscjf2203@gmail.com>
2023-08-16 23:45:18 +0900

    1. 글 작성하는 페이지 대략적인 뼈대 작성 2. 로컬호스트 접속되는지 간단히 테스트

--- src/main/java/com/test/controller/BoardController.java
+++ src/main/java/com/test/controller/BoardController.java
@@ -7,9 +7,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller // 스프링이 해당 클래스가 컨트롤러임을 인식하게 하기 위한 애너테이션
 public class BoardController {
 
-    @GetMapping("/")
-    @ResponseBody // GetMapping 상의 경로에 들어왔을때 브라우저에 출력해 주는 애너테이션
-    public String main() {
-        return "Test!!";
+    @GetMapping("/board/write") // localhost:3000/board/write
+    public String boardWriteForm() {
+
+        return "boardwrite";
     }
 }
