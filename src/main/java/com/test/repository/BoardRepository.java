dda6012c0fe6f81ce436474ade86ab001f4f0c5f
kmc2203 <alscjf2203@gmail.com>
2023-08-18 18:58:10 +0900

    웹에서 데이터가 잘 전송되는지 sout으로 간단히 확인

---
+++ src/main/java/com/test/repository/BoardRepository.java
@@ -0,0 +1,11 @@
+/**레포지토리!*/
+package com.test.repository;
+
+import com.test.entity.Board;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BoardRepository extends JpaRepository<Board, Integer> {
+
+}
---
+++ src/main/java/com/test/service/BoardService.java
@@ -0,0 +1,17 @@
+/**서비스는 컨트롤러에서 이용*/
+package com.test.service;
+
+import com.test.entity.Board;
+import com.test.repository.BoardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BoardService {
+    @Autowired
+    private BoardRepository boardRepository;
+
+    public void boardWrite(Board board) {
+        boardRepository.save(board);
+    }
+}
